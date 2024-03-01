2dd469e2cb4c27f52b0531b0f47d6f2c58aaf358
nicolas-martin <nicolas.martin@coinbase.com>
2024-03-01 13:39:01 -0500

    fiverr

--- content.js
+++ content.js
@@ -1,7 +1,43 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action == 'extractText') {
-    console.log('???');
-    // let text = document.getElementById('guide-body').innerText;
-    sendResponse({"text":"abc"});
-  }
-});
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === 'copyBioData') {
+        var xPathBioData = {
+            name: document.evaluate("//p[@id='name']/span[@id='nameValue']", document, null, 0, null)
+                    .iterateNext().textContent,
+            username: document.evaluate("//p[@id='username']/span[@id='usernameValue']", document, null, 0, null)
+                    .iterateNext().textContent,
+            email: document.evaluate("//p[@id='email']/span[@id='emailValue']", document, null, 0, null)
+                    .iterateNext().textContent,
+            contact: document.evaluate("//p[@id='contact']/span[@id='contactValue']", document, null, 0, null)
+                    .iterateNext().textContent,
+            address: document.evaluate("//p[@id='address']/span[@id='addressValue']", document, null, 0, null)
+                    .iterateNext().textContent
+        };
+        var cssSelectorBioData = {
+            name: document.getElementById('nameValue').innerText,
+            username: document.getElementById('usernameValue').innerText,
+            email: document.getElementById('emailValue').innerText,
+            contact: document.getElementById('contactValue').innerText,
+            address: document.getElementById('addressValue').innerText
+        };
+          
+        chrome.storage.sync.set({cssSelectorBioData:cssSelectorBioData, xPathBioData: xPathBioData})
+    }
+    else if (message.action === 'pasteBioData') {
+        chrome.storage.sync.get(["cssSelectorBioData", "xPathBioData"], function(result) {
+            if (result.xPathBioData) {
+                document.evaluate("//div[@class='form-group']/input[@id='name']", document, null, 0, null).iterateNext().value = result.xPathBioData.name;
+                document.evaluate("//div[@class='form-group']/input[@id='username']", document, null, 0, null).iterateNext().value = result.xPathBioData.username;
+                document.evaluate("//div[@class='form-group']/input[@id='email']", document, null, 0, null).iterateNext().value = result.xPathBioData.email;
+                document.evaluate("//div[@class='form-group']/input[@id='contact']", document, null, 0, null).iterateNext().value = result.xPathBioData.contact;
+                document.evaluate("//div[@class='form-group']/input[@id='address']", document, null, 0, null).iterateNext().value = result.xPathBioData.address;
+            }
+            if (result.cssSelectorBioData) {
+                document.getElementById('name').value = result.cssSelectorBioData.name;
+                document.getElementById('username').value = result.cssSelectorBioData.username;
+                document.getElementById('email').value = result.cssSelectorBioData.email;
+                document.getElementById('contact').value = result.cssSelectorBioData.contact;
+                document.getElementById('address').value = result.cssSelectorBioData.address;
+            }
+        })
+    }
+  });
--- popup.js
+++ popup.js
@@ -1,12 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var extractTextButton = document.getElementById('extractTextButton');
-
-  extractTextButton.addEventListener('click', function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { "action": 'extractText' }, function (response){
-        console.log(response);
-        alert(response);
-			});
-    });
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('copyButton').addEventListener('click', copyBioData);
+  document.getElementById('pasteButton').addEventListener('click', pasteBioData);
 });
+
+function copyBioData() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: 'copyBioData'});
+  });
+}
+
+function pasteBioData() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: 'pasteBioData'});
+  });
+}
